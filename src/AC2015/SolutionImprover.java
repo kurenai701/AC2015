369eb0f5d0b2f91a343c06f7372d573fc91c5137
kurenai701 <kurenai701@gmail.com>
2015-03-28 01:27:00 +0100

    Solution Improver

---
+++ src/AC2015/SolutionImprover.java
@@ -0,0 +1,86 @@
+package AC2015;
+
+import java.util.Random;
+
+public class SolutionImprover {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public SolutionImprover()
+	{
+
+	}
+
+	public Solution TryImprove(Solution oldSol)
+	{
+		// Deep copy is necessary so as not to mess up "old sol"
+		Solution newSol = Common.DeepCopy(oldSol);
+
+		int scoreOld = oldSol.GetScore();
+		//////////////////////////////////////
+		// TODO Code logic to improve solution
+		//////////////////////////////////////
+		
+		
+		
+		
+		//////////////////////////////////////
+		return newSol;
+
+
+	}
+
+
+	public Solution IterateImprover(Solution initSol, int nbIterations, int acceptIterationNoImprove)
+	{
+		Solution solCurrent = initSol;
+		Solution solTry;	
+
+		Solution bestSolution = Common.DeepCopy(initSol);
+
+		int countIterNoImprove = 0;
+
+		// itérer
+		for (int nIter = 0; nIter <= nbIterations; nIter++)
+		{
+			solTry = TryImprove(solCurrent);
+
+			// Si on dépasse notre "meilleur score", procéder à sauvegarde de cette best sol.
+			if (solTry.GetScore() > bestSolution.GetScore())
+			{				
+				bestSolution = Common.DeepCopy(solTry);
+			
+				// Serialize best solution in path = Common.ACFileFolderPath+fileName
+				bestSolution.SaveSolutionAsRaw("BestSolutionInProcess");
+			}
+			else
+			{
+				countIterNoImprove++; // incrémentation nb itération sans amélioration
+			}
+
+			// ALWAYS "shift".   TODO (add some random condition ?) TODO
+			solCurrent = solTry;	
+
+		
+			
+			
+			
+			// au bout d'un certain nombre d'itérations sans amélioration, repartir de la best solution
+			if (countIterNoImprove > acceptIterationNoImprove)
+			{
+				solCurrent = Common.DeepCopy(bestSolution);
+			}
+		}
+		
+		return bestSolution;
+	}
+
+	
+	
+	
+	
+
+}
